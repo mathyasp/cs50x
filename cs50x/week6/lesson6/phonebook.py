a6cb81a4cdb9a0b642f720c758391e9ab77cd161
mathyasp <119142489+mathyasp@users.noreply.github.com>
2023-01-19 22:45:14 +0000

    Reorganize current directories

--- cs50x/week6/lesson6/phonebook.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("phonebook.csv", "a") as file:
-
-    name = input("Name: ")
-    number = input("Number: ")
-
-    writer = csv.DictWriter(file, fieldnames=["name", "number"])
-    writer.writerow({"name": name, "number": number})
